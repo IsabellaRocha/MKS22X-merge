2ac2c178f34f29bd41227e958641c1d961ea785e
Isabella Rocha <irocha00@stuy.edu>
2019-03-19 14:51:11 -0400

    Insertoin

--- Merge.java
+++ Merge.java
@@ -7,6 +7,9 @@ public class Merge {
     if (start >= end) {
       return;
     }
+    if (end - start < 10) {
+      insertionsort(data, start, end);
+    }
     int[] temp = new int[data.length / 2];
     int[] temp2 = new int[data.length - data.length / 2];
     for (int idx = 0; idx < temp.length; idx++) {
@@ -41,30 +44,50 @@ public class Merge {
       }
     }
   }
-  private static void mergesortOpt(int[] data) {
-    int[] temp = new int[data.length / 2];
-    int[] temp2 = new int[data.length - data.length / 2];
-    mergesortOpt(data, temp, 0, data.length - 1);
-    mergesortOpt(data, temp2, 0, data.length - 1);
-    merge(temp, temp2);
+  public static void insertionsort(int[] ary, int start, int end) {
+    for (int idx = start + 1; idx < end; idx++) {
+      int current = ary[idx]; // Storing value for later to move
+      int curIdx = idx - 1;
+      while (curIdx >= 0 && ary[curIdx] > current) {
+        ary[curIdx + 1] = ary[curIdx]; // Shifting values over
+        curIdx--;
+      }
+      ary[curIdx + 1] = current;
+    //  System.out.println(Arrays.toString(ary));
+    }
   }
-  private static void mergesortOpt(int[] data, int[] temp, int start, int end) {
-
-  public static void main(String[] args) {
-    int[] ary = {2, 10, 15, 23, 0,  5};
-    int[] ary2 = {999,999,999,4,1,0,3,2,999,999,999};
-    int[] ary3 = {17,61,67,47,93,12,20,4,44,78};
-    int[] ary4 = {1, 2, 3, 4, 5, 6, 7, 8};
-    int[] ary5 = {3, 7, 15, -6, 34536456, 90, 39, 15, 5};
-    mergesort(ary);
-    System.out.println(Arrays.toString(ary));
-    mergesort(ary2);
-    System.out.println(Arrays.toString(ary2));
-    mergesort(ary3);
-    System.out.println(Arrays.toString(ary3));
-    mergesort(ary4);
-    System.out.println(Arrays.toString(ary4));
-    mergesort(ary5);
-    System.out.println(Arrays.toString(ary5));
+  public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Merge.mergesort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
   }
+}
 }
